0dda9b2e0f5e3a720c97189de56bd425b61ade19
arshivam <shivam.armarkar007@gmail.com>
2022-03-07 09:30:43 +0530

    Refactored the code and passed all Tests

--- test/java/HotelReservationTest.java
+++ test/java/HotelReservationTest.java
@@ -1,4 +1,3 @@
-import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,6 +11,27 @@ public class HotelReservationTest {
 		  String hotelName = hotelReservation.hotelList.get(0).getHotelName();
 	    Assert.assertEquals("LakeWood", hotelName);
 	}
+	@Test
+	public void GivenRates_shouldReturnProperRates() {
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("LakeWood","RegularCustomer",110,80);
+		int hotelRate = hotelReservation.hotelList.get(0).getWeekDayRates();
+	    Assert.assertEquals(110, hotelRate);
+	}
 	
-
+	@Test
+	public void GivenWeekEndRates_shouldReturnProperRates() {
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("LakeWood","RegularCustomer",110,80);
+		int hotelRate = hotelReservation.hotelList.get(0).getWeekEndRates();
+	    Assert.assertEquals(80, hotelRate);
+	}
+	
+	@Test
+	public void GivenCustomer_shouldReturnProperCustomerType() {
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("LakeWood","RegularCustomer",110,80);
+		String customerType = hotelReservation.hotelList.get(0).getCustomerType();
+	    Assert.assertEquals("RegularCustomer", customerType);
+	}
 }
